f5e7c7331f5dd6b5b91966d50073b83067bb676e
Sebastián Gómez Letelier <sgl1994@gmail.com>
2021-10-02 19:53:31 -0300

    III commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+
+import Login from '@/views/Login.vue'
+import Home from '@/views/Home.vue'
+import About from '@/views/About.vue'
 
 Vue.use(Router)
 
@@ -8,8 +11,21 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: 'Login',
+      component: Login
+    },
+    {
+      path: 'about',
+      redirect: 'About',
+      component: About
+    },
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      meta:{
+        login: true
+      }
     }
   ]
 })
