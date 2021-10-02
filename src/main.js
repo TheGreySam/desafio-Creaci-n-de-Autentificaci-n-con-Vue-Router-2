6aadb92e814a4be054340b09b08304c6cb443398
Sebastián Gómez Letelier <sgl1994@gmail.com>
2021-10-02 20:09:23 -0300

    IV commit

--- src/main.js
+++ src/main.js
@@ -3,12 +3,23 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import Element from 'element-ui'
+import BootstrapVue from 'bootstrap-vue'
+import 'element-ui/lib/theme-chalk/index.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import Vuex from 'vuex';
+
+Vue.use(Vuex);
+Vue.use(BootstrapVue);
+Vue.use(Element);
 
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store,
   router,
   components: { App },
   template: '<App/>'
