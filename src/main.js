579a0ce6a448e4f0651919c5478cc85896a85d99
Sebastián Gómez Letelier <sgl1994@gmail.com>
2021-10-02 21:11:24 -0300

    V commit

--- src/main.js
+++ src/main.js
@@ -1,13 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+import { BootstrapVue } from 'bootstrap-vue'
+import './plugins/bootstrap-vue'
+
+// Import Bootstrap an BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import App from './App'
 import router from './router'
 import Element from 'element-ui'
-import BootstrapVue from 'bootstrap-vue'
+
 import 'element-ui/lib/theme-chalk/index.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+
 import Vuex from 'vuex';
 
 Vue.use(Vuex);
